96398a92e34949c28c9fe38748d2c75e756c4bb1
yuchengtao <1281531720@qq.com>
2021-01-13 18:23:29 +0800

    init project

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const a = ref(0);
+    return {
+      a
+    };
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" :a="'ddd'" />
+    {{ a }}
+    <button @click="add">增加</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    HelloWorld
+  }
+})
+export default class Home extends Vue {
+  a = 123;
+  add = () => {
+    this.a++;
+  };
+}
+</script>
