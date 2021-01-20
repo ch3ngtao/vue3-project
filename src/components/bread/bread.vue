546050eb0369e3a6f36f1f75e51c3ce3da038625
yuchengtao <1281531720@qq.com>
2021-01-20 18:36:16 +0800

    添加答题页面

---
+++ src/components/bread/bread.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <a-breadcrumb>
+      <a-breadcrumb-item>Home</a-breadcrumb-item>
+      <a-breadcrumb-item><a href="">Application Center</a></a-breadcrumb-item>
+      <a-breadcrumb-item><a href="">Application List</a></a-breadcrumb-item>
+      <a-breadcrumb-item>An Application</a-breadcrumb-item>
+    </a-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss"></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -17,13 +16,9 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import("@/views/userCenter/userCenter.vue")
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/userTest",
+    name: "userTest",
+    component: () => import("@/views/userTest/userTest.vue")
   }
 ];
 
